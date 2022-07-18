28ac6cf08d096b629d3eb158d1b2c6e898342304
NDICHU <ndichumuriithi@gmail.com>
2022-07-18 21:23:02 +0300

    commit tree 37eada479df9b6e9cca81e9f5518ceb8ba8fd90a

---
+++ .history/app/controllers/students_controller_20220718212232.rb
@@ -0,0 +1,25 @@
+class StudentsController < ApplicationController
+
+  def index
+    students = Student.all
+    render json: students
+
+    students = if params[:name]
+                 Student.by_name(params[:name])
+               else
+                 Student.all
+               end    
+    render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+  end
+  def show
+    student = Student.find_by(id: params[:id])
+    render json: student
+  end
+
+end
